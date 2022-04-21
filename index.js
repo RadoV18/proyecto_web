b677ed19f3e51014f2800ceda042cc6c74b74eca
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-04-21 10:48:33 -0400

    Configuracion backend

---
+++ index.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+const mongooseURI = process.env.DATABASE_URI;
+mongoose.connect(mongooseURI)
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('error connecting to MongoDB', err);
+    })
+
+app.use(morgan('tiny'));
+app.use(express.json())
+
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('An error occurred');
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
